71c5ebeb0c9937d06cc51659fafbed899540bc6c
Grigorii Matiukhin <contact@gmatiukhin.site>
2023-12-19 11:23:59 +0300

    ppo from scratch

---
+++ fsm/ppo-from-scratch/ppo.py
@@ -0,0 +1,171 @@
+import torch
+from torch import nn
+from torch.distributions import MultivariateNormal
+from torch.optim import Adam
+import numpy as np
+import gymnasium as gym
+
+from network import FeedForwardNN
+
+
+class PPO:
+    def __init__(self, env, **hyperparameters) -> None:
+        assert type(env.observation_space) == gym.spaces.Box
+        assert type(env.action_space) == gym.spaces.Box
+
+        self.env = env
+        self.obs_dim = env.observation_space.shape[0]
+        self.act_dim = env.action_space.shape[0]
+
+        # Initialize actor and critic networks
+        self.actor = FeedForwardNN(self.obs_dim, self.act_dim)
+        self.critic = FeedForwardNN(self.obs_dim, 1)
+
+        self._init_hyperparameters(hyperparameters)
+
+        # Our covariance matrix
+        self.cov_var = torch.full(size=(self.act_dim,), fill_value=0.5)
+        self.cov_mat = torch.diag(self.cov_var)
+
+        self.actor_optim = Adam(self.actor.parameters(), lr=self.lr)
+        self.critic_optim = Adam(self.critic.parameters(), lr=self.lr)
+
+    def _init_hyperparameters(self, hyperparameters):
+        # NOTE: these are default values chosen arbitrarily, will need to change later
+        self.timesteps_per_batch = 4800
+        self.max_timesteps_per_episode = 1600
+        self.gamma = 0.95
+        self.n_updates_per_iteration = 5
+        self.clip = 0.2  # As recommended by the paper
+        self.lr = 0.005
+
+        for param, val in hyperparameters.items():
+            exec("self." + param + " = " + str(val))
+
+    def learn(self, total_timesteps):
+        t_so_far = 0
+        while t_so_far < total_timesteps:
+            print(t_so_far, "--->", total_timesteps)
+            (
+                batch_obs,
+                batch_acts,
+                batch_log_probs,
+                batch_rtgs,
+                batch_lens,
+            ) = self.rollout()
+
+            t_so_far += np.sum(batch_lens)
+
+            V, _ = self.evaluate(batch_obs, batch_acts)
+            A_k = batch_rtgs - V.detach()
+            # Normalize advantages
+            A_k = (A_k - A_k.mean()) / (A_k.std() - 1e-10)
+
+            for _ in range(self.n_updates_per_iteration):
+                V, curr_log_probs = self.evaluate(batch_obs, batch_acts)
+                ratios = torch.exp(curr_log_probs - batch_log_probs)
+
+                # Calculate surrogate losses
+                surr1 = ratios * A_k
+                surr2 = torch.clamp(ratios, 1 - self.clip, 1 + self.clip) * A_k
+
+                # Calculate actor and critic losses.
+                # NOTE: we take the negative min of the surrogate losses
+                # because we're trying to maximize the performance function,
+                # but Adam minimizes the loss. So minimizing the negative
+                # performance function maximizes it.
+                actor_loss = (-torch.min(surr1, surr2)).mean()
+                critic_loss = nn.MSELoss()(V, batch_rtgs)
+
+                # Calculate gradients and perform backward propagation for actor network
+                self.actor_optim.zero_grad()
+                actor_loss.backward(retain_graph=True)
+                self.actor_optim.step()
+
+                # Calculate gradients and perform backward propagation for critic network
+                self.critic_optim.zero_grad()
+                critic_loss.backward()
+                self.critic_optim.step()
+
+        torch.save(self.actor.state_dict(), "./ppo_actor.pth")
+        torch.save(self.critic.state_dict(), "./ppo_critic.pth")
+
+    def evaluate(self, batch_obs, batch_acts):
+        # Query critic network for a value V for each obs in batch_obs
+        V = self.critic(batch_obs).squeeze()
+
+        # Calculate the log probabilities of batch actions using most recent actor network.
+        mean = self.actor(batch_obs)
+        dist = MultivariateNormal(mean, self.cov_mat)
+        log_probs = dist.log_prob(batch_acts)
+
+        return V, log_probs
+
+    def get_action(self, obs):
+        # Query the actor network for a mean action.
+        # Same thing as calling self.actor.forward(obs)
+        mean = self.actor(obs)
+
+        dist = MultivariateNormal(mean, self.cov_mat)
+
+        # Sample an action from the distribution and get its log prob
+        action = dist.sample()
+        log_prob = dist.log_prob(action)
+
+        return action.detach().numpy(), log_prob.detach()
+
+    def compute_rtgs(self, batch_rews):
+        batch_rtgs = []
+
+        for ep_rews in reversed(batch_rews):
+            discounted_reward = 0
+            for rew in reversed(ep_rews):
+                discounted_reward = rew + discounted_reward * self.gamma
+                batch_rtgs.insert(0, discounted_reward)
+
+        batch_rtgs = torch.tensor(batch_rtgs, dtype=torch.float)
+
+        return batch_rtgs
+
+    def rollout(self):
+        batch_obs = []
+        batch_acts = []
+        batch_log_probs = []
+        batch_rews = []
+        batch_rtgs = []
+        batch_lens = []
+
+        t = 0
+        while t < self.timesteps_per_batch:
+            ep_rews = []
+
+            obs, _ = self.env.reset()
+            done = False
+
+            ep_t = 0
+            for ep_t in range(self.max_timesteps_per_episode):
+                t += 1
+
+                batch_obs.append(obs)
+
+                # action = self.env.action_space.sample()
+                action, log_prob = self.get_action(obs)
+                obs, rew, done, _, _ = self.env.step(action)
+
+                ep_rews.append(rew)
+                batch_acts.append(action)
+                batch_log_probs.append(log_prob)
+
+                if done:
+                    break
+
+            batch_lens.append(ep_t + 1)
+            batch_rews.append(ep_rews)
+
+        batch_obs = torch.tensor(batch_obs, dtype=torch.float)
+        batch_acts = torch.tensor(batch_acts, dtype=torch.float)
+        batch_log_probs = torch.tensor(batch_log_probs, dtype=torch.float)
+
+        batch_rtgs = self.compute_rtgs(batch_rews)
+
+        return batch_obs, batch_acts, batch_log_probs, batch_rtgs, batch_lens
